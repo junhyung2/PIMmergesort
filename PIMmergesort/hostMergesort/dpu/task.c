81c0c2f6514f62cb0155934907c38f7956f311fb
junhyung2 <peter047738@gmail.com>
2025-07-08 14:15:48 +0900

    Update task.c

--- PIMmergesort/hostMergesort/dpu/task.c
+++ PIMmergesort/hostMergesort/dpu/task.c
@@ -33,48 +33,6 @@ void __attribute__((noinline)) insertionsort(T *input, unsigned int l_size) {
   }
 }
 
-// merge 함수 -> host merge이므로 필요없음
-
-//  void Merge(int* unsorted, uint32_t left, uint32_t mid, uint32_t right,
-//             int* sorted) {
-//    uint32_t i = left;
-//    uint32_t j = mid;
-//    for (int k = left; k < right; k++) {
-//      if (i < mid && (j >= right || unsorted[i] <= unsorted[j])) {
-//        sorted[k] = unsorted[i];
-//        i += 1;
-//      } else {
-//        sorted[k] = unsorted[j];
-//        j += 1;
-//      }
-//    }
-//  }
-
-//  void CopyBufferBfromA(int* bufferA, int* bufferB, uint32_t size) {
-//    for (uint32_t idx = 0; idx < size; ++idx) {
-//      bufferB[idx] = bufferA[idx];
-//    }
-//  }
-
-//  void __attribute__((noinline)) iterativeMerge(int* bufferB, int* bufferA,
-//                                                unsigned int l_size) {
-//    bool isAtoB = true;
-//    for (uint32_t width = BLOCK_SIZE / sizeof(T); width < l_size; width *= 2)
-//    {
-//      for (uint32_t idx = 0; idx < l_size; idx += 2 * width) {
-//        if (isAtoB)
-//          Merge(bufferA, idx, Min(idx + width, l_size),
-//                Min(idx + 2 * width, l_size), bufferB);
-//        else
-//          Merge(bufferB, idx, Min(idx + width, l_size),
-//                Min(idx + 2 * width, l_size), bufferA);
-//      }
-//      isAtoB = !isAtoB;
-//    }
-//    if (!isAtoB) {
-//      CopyBufferBfromA(bufferB, bufferA, l_size);
-//    }
-//  }
 
 // Barrier 초기화
 BARRIER_INIT(my_barrier, NR_TASKLETS);
@@ -124,22 +82,6 @@ int main_kernel1() {
   result->cycles = timer_stop(&cycles);
 #endif
 
-  //  if (tasklet_id == 0) {
-  //    T* cache_B = (T*)mem_alloc(total_written_bytes);
-  //    T* cache_C = (T*)mem_alloc(total_written_bytes);
-
-  //    mram_read((__mram_ptr void const*)(mram_base_addr_A), cache_B,
-  //              total_written_bytes);
-
-  //    uint32_t num_total_elements = total_written_bytes >> DIV;
-
-  //    if (num_total_elements > 0) {
-  //      iterativeMerge(cache_B, cache_C, num_total_elements);
-  //    }
-
-  //    mram_write(cache_B, (__mram_ptr void*)(mram_base_addr_A),
-  //               total_written_bytes);
-  //  }
 #if PERF && !PERF_SYNC
   result->cycles = timer_stop(&cycles);
 #endif
